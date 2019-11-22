2d5c74ee03e0731641727b55a7ed2428197f234b
zhujunxi <531578783@qq.com>
2019-11-22 10:37:26 +0800

    feat: add website

--- src/views/tool/data/category/insight.js
+++ src/views/tool/data/category/insight.js
@@ -2,72 +2,76 @@
  * 数据洞察与指数
 */
 export default [
-    {
-      name: "七麦",
-      link: "https://www.qimai.cn/"
-    },
-    {
-      name: "有米-App-Growing",
-      link: "https://appgrowing.cn/"
-    },
-    {
-      name: "新榜有数",
-      link: "https://data.newrank.cn/"
-    },
-    {
-      name: "易观智库",
-      link: "https://growth.analysys.cn/dashboard/detail/399"
-    },
-    {
-      name: "神策数据",
-      link: "https://www.sensorsdata.cn"
-    },
-    {
-      name: "经管之家",
-      link: "https://bbs.pinggu.org/"
-    },
-    {
-      name: "QuestMobile",
-      link: "https://www.questmobile.com.cn/"
-    },
-    {
-      name: "烯牛数据",
-      link: "http://www.xiniudata.com/"
-    },
-    {
-      name: "Mob数据研究院",
-      link: "http://www.mob.com/mobdata/report"
-    },
-    {
-      name: "cbnData 第一财经商数中心 ★",
-      link: "http://www.cbndata.com"
-    },
-    {
-      name: "企鹅智酷",
-      link: "https://re.qq.com/"
-    },
-    {
-      name: "艾瑞网",
-      link: "https://www.iresearch.cn/"
-    },
-    {
-      name: "IBM商业价值研究院",
-      link: "https://www.ibm.com/cn-zh/services/insights/institute-business-value"
-    },
-    {
-      name: "腾讯位置大数据",
-      link: "https://heat.qq.com/"
-    },
-    {
-      name: "发现报告",
-      link: "https://www.fxbaogao.com"
-    },
-    {
-      name: "国家统计局年鉴数据",
-      link: "http://www.stats.gov.cn/tjsj/ndsj/2017/indexch.htm"
-    },
-    {
-      name: "中国餐饮协会-数据报告",
-      link: "http://www.chinahotel.org.cn/forward/enterSecondDary.do?id=4a41851c14184c9495f3aad314fc4290&childMId1=4e28ce0583794d08a63c4036d336f5cc&moduleId=4e28ce0583794d08a63c4036d336f5cc"
-    }
-  ]
+  {
+    name: "七麦",
+    link: "https://www.qimai.cn/"
+  },
+  {
+    name: "有米-App-Growing",
+    link: "https://appgrowing.cn/"
+  },
+  {
+    name: "新榜有数",
+    link: "https://data.newrank.cn/"
+  },
+  {
+    name: "易观智库",
+    link: "https://growth.analysys.cn/dashboard/detail/399"
+  },
+  {
+    name: "神策数据",
+    link: "https://www.sensorsdata.cn"
+  },
+  {
+    name: "清科研究院",
+    link: "https://www.pedata.cn/"
+  },
+  {
+    name: "经管之家",
+    link: "https://bbs.pinggu.org/"
+  },
+  {
+    name: "QuestMobile",
+    link: "https://www.questmobile.com.cn/"
+  },
+  {
+    name: "烯牛数据",
+    link: "http://www.xiniudata.com/"
+  },
+  {
+    name: "Mob数据研究院",
+    link: "http://www.mob.com/mobdata/report"
+  },
+  {
+    name: "cbnData 第一财经商数中心 ★",
+    link: "http://www.cbndata.com"
+  },
+  {
+    name: "企鹅智酷",
+    link: "https://re.qq.com/"
+  },
+  {
+    name: "艾瑞网",
+    link: "https://www.iresearch.cn/"
+  },
+  {
+    name: "IBM商业价值研究院",
+    link: "https://www.ibm.com/cn-zh/services/insights/institute-business-value"
+  },
+  {
+    name: "腾讯位置大数据",
+    link: "https://heat.qq.com/"
+  },
+  {
+    name: "发现报告",
+    link: "https://www.fxbaogao.com"
+  },
+  {
+    name: "国家统计局年鉴数据",
+    link: "http://www.stats.gov.cn/tjsj/ndsj/2017/indexch.htm"
+  },
+  {
+    name: "中国餐饮协会-数据报告",
+    link: "http://www.chinahotel.org.cn/forward/enterSecondDary.do?id=4a41851c14184c9495f3aad314fc4290&childMId1=4e28ce0583794d08a63c4036d336f5cc&moduleId=4e28ce0583794d08a63c4036d336f5cc"
+  }
+]
