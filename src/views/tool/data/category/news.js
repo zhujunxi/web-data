469818c0e40212e7097b8a7455ad325f09dde50c
zhujunxi <531578783@qq.com>
2020-01-07 17:18:57 +0800

    chore: add format-code tool

--- src/views/tool/data/category/news.js
+++ src/views/tool/data/category/news.js
@@ -1,78 +1,80 @@
 /**
  * 资讯
-*/
+ */
 export default [
-  {
-    name: "36氪",
-    link: "https://36kr.com/",
-    logo:
-      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1573741193726&di=8cb38619a078a2b0a1c00af61c1f416d&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2Fb8ab63f23c5c2daccf4d3dc58ba8ad4a20a92b7110377-GSjq7f_fw658"
-  },
-  {
-    name: "虎嗅",
-    link: "https://www.huxiu.com/",
-    logo: ""
-  },
-  {
-    name: "星球日报",
-    link: "https://www.odaily.com/"
-  },
-  {
-    name: "财新",
-    link: "http://www.caixin.com/"
-  },
-  {
-    name: "猎云网",
-    link: "https://www.lieyunwang.com/"
-  },
-  {
-    name: "PM-CAFF",
-    link: "https://coffee.pmcaff.com/"
-  },
-  {
-    name: "鸟哥笔记",
-    link: "https://www.niaogebiji.com/"
-  },
-  {
-    name: "笔记侠",
-    link: "https://www.bijixia.net"
-  },
-  {
-    name: "数英网",
-    link: "https://www.digitaling.com/"
-  },
-  {
-    name: "运营派",
-    link: "https://www.yunyingpai.com/"
-  },
-  {
-    name: "华杉的新浪博客",
-    link: "http://blog.sina.com.cn/hua20081231"
-  },
-  {
-    name: "青瓜传媒",
-    link: "http://www.opp2.com/"
-  },
-  {
-    name: "钛媒体",
-    link: "https://www.tmtpost.com/"
-  },
-  {
-    name: "数据分析网",
-    link: "https://www.afenxi.com/"
-  },
-  {
-    name: "商业人物",
-    link: "http://zixun.582582.com/tag/fangtan"
-  },
-  {
-    name: "人人都是产品经理",
-    link: "http://www.woshipm.com/"
-  }, {
-    name: "前瞻经济学人",
-    link: "https://t.qianzhan.com/"
-  }, {
-    name: "V2EX",
-    link: "https://www.v2ex.com/"
-  }
+    {
+        name: '36氪',
+        link: 'https://36kr.com/',
+        logo:
+            'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1573741193726&di=8cb38619a078a2b0a1c00af61c1f416d&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2Fb8ab63f23c5c2daccf4d3dc58ba8ad4a20a92b7110377-GSjq7f_fw658'
+    },
+    {
+        name: '虎嗅',
+        link: 'https://www.huxiu.com/',
+        logo: ''
+    },
+    {
+        name: '星球日报',
+        link: 'https://www.odaily.com/'
+    },
+    {
+        name: '财新',
+        link: 'http://www.caixin.com/'
+    },
+    {
+        name: '猎云网',
+        link: 'https://www.lieyunwang.com/'
+    },
+    {
+        name: 'PM-CAFF',
+        link: 'https://coffee.pmcaff.com/'
+    },
+    {
+        name: '鸟哥笔记',
+        link: 'https://www.niaogebiji.com/'
+    },
+    {
+        name: '笔记侠',
+        link: 'https://www.bijixia.net'
+    },
+    {
+        name: '数英网',
+        link: 'https://www.digitaling.com/'
+    },
+    {
+        name: '运营派',
+        link: 'https://www.yunyingpai.com/'
+    },
+    {
+        name: '华杉的新浪博客',
+        link: 'http://blog.sina.com.cn/hua20081231'
+    },
+    {
+        name: '青瓜传媒',
+        link: 'http://www.opp2.com/'
+    },
+    {
+        name: '钛媒体',
+        link: 'https://www.tmtpost.com/'
+    },
+    {
+        name: '数据分析网',
+        link: 'https://www.afenxi.com/'
+    },
+    {
+        name: '商业人物',
+        link: 'http://zixun.582582.com/tag/fangtan'
+    },
+    {
+        name: '人人都是产品经理',
+        link: 'http://www.woshipm.com/'
+    },
+    {
+        name: '前瞻经济学人',
+        link: 'https://t.qianzhan.com/'
+    },
+    {
+        name: 'V2EX',
+        link: 'https://www.v2ex.com/'
+    }
 ]
--- src/views/tool/index.vue
+++ src/views/tool/index.vue
@@ -1,156 +1,158 @@
 <template>
-	<div class="tool-home">
-		<Banner></Banner>
-		<div class="tool-item-warp">
-			<div class="tool-item" v-for="item,index of toolData" :key="index">
-				<div class="tool-item-hd">{{item.label}}</div>
-				<div class="tool-item-bd">
-					<el-row :gutter="20">
-						<el-col :span="4" v-for="citem,cindex of item.resource" :key="cindex">
-							<template v-if="citem.link">
-								<a :href="citem.link" class="tool-cell" target="_blank" :title="citem.name">
-									<img
-										class="tool-cell-logo"
-										:src="citem.logo"
-										v-if="citem.logo"
-									/>
-									<div class="tool-cell-logo-text" v-else :style="getLogoStyle()">{{citem.name | namaTransformLogo}}</div>
-									<div class="tool-cell-name">{{citem.name}}</div>
-								</a>
-							</template>
-							<el-popover placement="top" trigger="hover" v-else-if="citem.QRCode">
-								<img
-									class="tool-popover-img"
-									v-if="!citem.link"
-									:src="require('@/assets/res/'+ citem.QRCode)"
-									alt
-									srcset
-								/>
-								<a slot="reference" class="tool-cell">
-									<div class="tool-cell-logo" :style="getLogoStyle()">{{citem.name | namaTransformLogo}}</div>
-									<div class="tool-cell-name">{{citem.name}}</div>
-								</a>
-							</el-popover>
-							<el-tooltip class="item" :content="citem.tip" effect="light" v-else-if="citem.tip">
-								<a class="tool-cell">
-									<div class="tool-cell-logo" :style="getLogoStyle()">{{citem.name | namaTransformLogo}}</div>
-									<div class="tool-cell-name">{{citem.name}}</div>
-								</a>
-							</el-tooltip>
-						</el-col>
-					</el-row>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="tool-home">
+        <Banner></Banner>
+        <div class="tool-item-warp">
+            <div class="tool-item" v-for="(item, index) of toolData" :key="index">
+                <div class="tool-item-hd">{{ item.label }}</div>
+                <div class="tool-item-bd">
+                    <el-row :gutter="20">
+                        <el-col :span="4" v-for="(citem, cindex) of item.resource" :key="cindex">
+                            <template v-if="citem.link">
+                                <a :href="citem.link" class="tool-cell" target="_blank" :title="citem.name">
+                                    <img class="tool-cell-logo" :src="citem.logo" v-if="citem.logo" />
+                                    <div class="tool-cell-logo-text" v-else :style="getLogoStyle()">
+                                        {{ citem.name | namaTransformLogo }}
+                                    </div>
+                                    <div class="tool-cell-name">{{ citem.name }}</div>
+                                </a>
+                            </template>
+                            <el-popover placement="top" trigger="hover" v-else-if="citem.QRCode">
+                                <img
+                                    class="tool-popover-img"
+                                    v-if="!citem.link"
+                                    :src="require('@/assets/res/' + citem.QRCode)"
+                                    alt
+                                    srcset
+                                />
+                                <a slot="reference" class="tool-cell">
+                                    <div class="tool-cell-logo" :style="getLogoStyle()">
+                                        {{ citem.name | namaTransformLogo }}
+                                    </div>
+                                    <div class="tool-cell-name">{{ citem.name }}</div>
+                                </a>
+                            </el-popover>
+                            <el-tooltip class="item" :content="citem.tip" effect="light" v-else-if="citem.tip">
+                                <a class="tool-cell">
+                                    <div class="tool-cell-logo" :style="getLogoStyle()">
+                                        {{ citem.name | namaTransformLogo }}
+                                    </div>
+                                    <div class="tool-cell-name">{{ citem.name }}</div>
+                                </a>
+                            </el-tooltip>
+                        </el-col>
+                    </el-row>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import Banner from "./components/banner"
+import Banner from './components/banner'
 
-import toolData from "./data/website";
-import { generateRgbaColor } from "@/utils/utils";
+import toolData from './data/website'
+import { generateRgbaColor } from '@/utils/utils'
 
 export default {
-	components: {
-		Banner
-	},
-	data() {
-		return {
-			toolData: toolData
-		};
-	},
-	filters: {
-		namaTransformLogo(value) {
-			let regNumber = /^[0-9]*/g;
-			let pureVal = value.replace(regNumber, "");
-			return pureVal.slice(0, 1);
-		}
-	},
-	methods: {
-		getLogoStyle() {
-			return `background:${generateRgbaColor()}`;
-		}
-	}
-};
+    components: {
+        Banner
+    },
+    data() {
+        return {
+            toolData: toolData
+        }
+    },
+    filters: {
+        namaTransformLogo(value) {
+            let regNumber = /^[0-9]*/g
+            let pureVal = value.replace(regNumber, '')
+            return pureVal.slice(0, 1)
+        }
+    },
+    methods: {
+        getLogoStyle() {
+            return `background:${generateRgbaColor()}`
+        }
+    }
+}
 </script>
 
 <style lang="less" scoped>
-@import "~@/assets/style/variable.less";
+@import '~@/assets/style/variable.less';
 .tool-home {
-	background: #f3f7fe;
-	width: 100%;
+    background: #f3f7fe;
+    width: 100%;
 }
 
 .tool-item-warp {
-	width: 1200px;
-	margin: 0 auto;
-	transform: translateY(-46px);
-	padding: 12px;
-	background: #fff;
-	border-radius: 2px;
-	box-shadow: 0 2px 4px rgba(0,0,0,0.05);
+    width: 1200px;
+    margin: 0 auto;
+    transform: translateY(-46px);
+    padding: 12px;
+    background: #fff;
+    border-radius: 2px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
 }
 .tool-item {
-	&-hd {
-		font-size: 16px;
-		font-weight: bold;
-		color: @primary-color;
-		display: flex;
-		justify-content: space-between;
-		padding: 10px 12px 10px 12px;
-		border-bottom: 1px solid rgba(0, 0, 0, 0.02);
-	}
-	&-bd {
-		padding: 24px 20px;
-	}
+    &-hd {
+        font-size: 16px;
+        font-weight: bold;
+        color: @primary-color;
+        display: flex;
+        justify-content: space-between;
+        padding: 10px 12px 10px 12px;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.02);
+    }
+    &-bd {
+        padding: 24px 20px;
+    }
 }
 .tool-popover-img {
-	width: 160px;
-	height: 160px;
+    width: 160px;
+    height: 160px;
 }
 .tool-cell {
-	color: #666;
-	background: #ecf5ff5c;
-	padding: 10px 8px;
-	display: flex;
-	align-items: center;
-	cursor: pointer;
-	text-decoration: none;
-	margin-bottom: 20px;
-	border-radius: 2px;
-	border: 1px solid rgba(0, 0, 0, 0.04);
-	transition: all 0.2s ease-in-out;
-	&-logo {
-		width: 28px;
-		height: 28px;
-		border-radius: 50px;
-	}
-	&-logo-text {
-		width: 28px;
-		height: 28px;
-		border-radius: 50px;
-		line-height: 28px;
-		text-align: center;
-		flex-shrink: 0;
-		font-size: 14px;
-		font-weight: bold;
-		color: #fff;
-		text-shadow: 0 1px 0px rgba(0, 0, 0, 0.1);
-	}
-	&-name {
-		padding-left: 6px;
-		font-size: 14px;
-		font-weight: bold;
-		white-space: nowrap;
-		overflow: hidden;
-		text-overflow: ellipsis;
-	}
-	&:hover {
-		color: @primary-color;
-		border: 1px solid rgba(0, 0, 0, 0.12);
-		box-shadow: 0px 9px 7px -5px rgba(0, 0, 0, 0.08);
-		transition: all 0.2s ease-in-out;
-	}
+    color: #666;
+    background: #ecf5ff5c;
+    padding: 10px 8px;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    text-decoration: none;
+    margin-bottom: 20px;
+    border-radius: 2px;
+    border: 1px solid rgba(0, 0, 0, 0.04);
+    transition: all 0.2s ease-in-out;
+    &-logo {
+        width: 28px;
+        height: 28px;
+        border-radius: 50px;
+    }
+    &-logo-text {
+        width: 28px;
+        height: 28px;
+        border-radius: 50px;
+        line-height: 28px;
+        text-align: center;
+        flex-shrink: 0;
+        font-size: 14px;
+        font-weight: bold;
+        color: #fff;
+        text-shadow: 0 1px 0px rgba(0, 0, 0, 0.1);
+    }
+    &-name {
+        padding-left: 6px;
+        font-size: 14px;
+        font-weight: bold;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+    &:hover {
+        color: @primary-color;
+        border: 1px solid rgba(0, 0, 0, 0.12);
+        box-shadow: 0px 9px 7px -5px rgba(0, 0, 0, 0.08);
+        transition: all 0.2s ease-in-out;
+    }
 }
 </style>
