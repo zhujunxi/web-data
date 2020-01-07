28e6f5e584e7448f82ccc18e26c8c1f51e84b724
zhujunxi <531578783@qq.com>
2020-01-07 17:20:20 +0800

    chore: add format-code tool

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    // 行尾逗号,默认none,可选 none|es5|all
+    // es5 包括es5中的数组、对象
+    // all 包括函数对象等所有可选
+    trailingComma: 'none',
+    // tab缩进大小,默认为2
+    tabWidth: 4,
+    // 使用分号, 默认true
+    semi: false,
+    // 使用单引号, 默认false(在jsx中配置无效, 默认都是双引号)
+    singleQuote: true,
+    // 换行字符串阈值
+    printWidth: 120,
+    // 对象&数组是否追加空格
+    bracketSpacing: true
+}
