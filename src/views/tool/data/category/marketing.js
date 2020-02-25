47769e245be5851d26bb074f3a981935d7738943
zhujunxi <531578783@qq.com>
2020-02-25 11:48:40 +0800

    feat: add website

--- src/views/tool/data/category/marketing.js
+++ src/views/tool/data/category/marketing.js
@@ -1,24 +1,29 @@
 /**
  * 营销
-*/
+ */
 export default [
-  {
-    name: "Advertcn广告中国",
-    link: "https://www.advertcn.com/"
-  },
-  {
-    name: "Morketing",
-    link: "https://www.morketing.com/"
-  },
-  {
-    name: "至顶网",
-    link: "http://www.zhiding.cn/"
-  }, {
-    name: "销售家-销帮",
-    link: "http://www.xiaoshoujia.com.cn/"
-  },
-  {
-    name: "SocialMarketing",
-    link: "https://www.socialmarketings.com/"
-  }
+    {
+        name: 'Advertcn广告中国',
+        link: 'https://www.advertcn.com/'
+    },
+    {
+        name: 'Morketing',
+        link: 'https://www.morketing.com/'
+    },
+    {
+        name: '至顶网',
+        link: 'http://www.zhiding.cn/'
+    },
+    {
+        name: '销售家-销帮',
+        link: 'http://www.xiaoshoujia.com.cn/'
+    },
+    {
+        name: 'SocialMarketing',
+        link: 'https://www.socialmarketings.com/'
+    },
+    {
+        name: '莫然博客',
+        link: 'https://seoxiaoxin.com/'
+    }
 ]
