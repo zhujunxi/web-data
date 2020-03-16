51929c180934f1df217ed0b917f80e33039f56f7
zhujunxi <531578783@qq.com>
2020-03-16 17:41:57 +0800

    feat: add website

--- src/views/tool/data/category/tool.js
+++ src/views/tool/data/category/tool.js
@@ -1,44 +1,49 @@
 /**
  * 工具
-*/
+ */
 export default [
-  {
-    name: "语雀",
-    link: "https://www.yuque.com/"
-  },
-  {
-    name: "微信公众平台",
-    link: "https://mp.weixin.qq.com/"
-  },
-  {
-    name: "有道云笔记",
-    link: "https://note.youdao.com"
-  },
-  {
-    name: "ProcessOn",
-    link: "https://www.processon.com"
-  },
-  {
-    name: "稿定设计",
-    link: "https://www.gaoding.com/"
-  },
-  {
-    name: "BDP-可视化数据分析",
-    link: "https://www.bdp.cn"
-  },
-  {
-    name: "阿里研究院",
-    link: "http://www.aliresearch.com"
-  },
-  {
-    name: "清博数据-网址导航",
-    link: "http://hao.gsdata.cn/"
-  },
-  {
-    name: "魔筷-电商选品",
-    link: "http://www.mockuai.com/"
-  }, {
-    name: "爆汁裂变",
-    link: "http://www.youdd.wang"
-  }
+    {
+        name: '语雀',
+        link: 'https://www.yuque.com/'
+    },
+    {
+        name: '微信公众平台',
+        link: 'https://mp.weixin.qq.com/'
+    },
+    {
+        name: '有道云笔记',
+        link: 'https://note.youdao.com'
+    },
+    {
+        name: 'ProcessOn',
+        link: 'https://www.processon.com'
+    },
+    {
+        name: '稿定设计',
+        link: 'https://www.gaoding.com/'
+    },
+    {
+        name: 'BDP-可视化数据分析',
+        link: 'https://www.bdp.cn'
+    },
+    {
+        name: '阿里研究院',
+        link: 'http://www.aliresearch.com'
+    },
+    {
+        name: '清博数据-网址导航',
+        link: 'http://hao.gsdata.cn/'
+    },
+    {
+        name: '魔筷-电商选品',
+        link: 'http://www.mockuai.com/'
+    },
+    {
+        name: '爆汁裂变',
+        link: 'http://www.youdd.wang'
+    },
+    {
+        name: '在线格式转换',
+        link: 'https://convertio.co/'
+    }
 ]
