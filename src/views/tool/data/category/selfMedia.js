0cf3935224f0ebe44bf256d71abaefa6cb434c0b
zhujunxi <531578783@qq.com>
2020-03-16 15:16:49 +0800

    feat: add website

--- src/views/tool/data/category/selfMedia.js
+++ src/views/tool/data/category/selfMedia.js
@@ -1,37 +1,41 @@
 /**
  * 自媒体
-*/
+ */
 export default [
-  {
-    name: "易撰",
-    link: "https://www.yizhuan5.com/"
-  },
-  {
-    name: "新榜",
-    link: "https://www.newrank.cn/"
-  },
-  {
-    name: "卡思数据",
-    link: "https://www.caasdata.com/index/rank/index.html"
-  },
-  {
-    name: "罗网",
-    link: "https://www.luonet.com"
-  },
-  {
-    name: "飞瓜数据",
-    link: "https://www.feigua.cn/"
-  },
-  {
-    name: "考拉新媒体导航",
-    link: "https://www.kaolamedia.com"
-  },
-  {
-    name: "99抖商",
-    link: "http://www.doushang99.com/"
-  },
-  {
-    name: "领库排行榜",
-    link: "https://www.kolrank.com/"
-  }
+    {
+        name: '易撰',
+        link: 'https://www.yizhuan5.com/'
+    },
+    {
+        name: '新榜',
+        link: 'https://www.newrank.cn/'
+    },
+    {
+        name: '卡思数据',
+        link: 'https://www.caasdata.com/index/rank/index.html'
+    },
+    {
+        name: '罗网',
+        link: 'https://www.luonet.com'
+    },
+    {
+        name: '飞瓜数据',
+        link: 'https://www.feigua.cn/'
+    },
+    {
+        name: '考拉新媒体导航',
+        link: 'https://www.kaolamedia.com'
+    },
+    {
+        name: '摸鱼导航',
+        link: 'https://www.moyu.link'
+    },
+    {
+        name: '99抖商',
+        link: 'http://www.doushang99.com/'
+    },
+    {
+        name: '领库排行榜',
+        link: 'https://www.kolrank.com/'
+    }
 ]
